b8d47772e0d348772869ca1c7f8a4415156d3185
Deeksha <deeksha.sharma25@gmail.com>
2016-03-06 20:01:10 -0800

    Test added for roll

--- src/test/java/rsync/RollingChecksumTest.java
+++ src/test/java/rsync/RollingChecksumTest.java
@@ -25,10 +25,10 @@ public class RollingChecksumTest {
         int actualEndIndex = checksum.getEndIndex();
 
 
-        int expectedFirst16 = 343;
+        int expectedFirst16 = 343 % Constants.MOD_M;
         int actualFirst16 = checksum.getFirst16Bit();
 
-        int expectedSecond16 = 819;
+        int expectedSecond16 = 819 % Constants.MOD_M;
         int actualSecond16 = checksum.getSecond16Bit();
 
         assertTrue(Arrays.equals(expectedBlock,actualBlock));
@@ -47,7 +47,9 @@ public class RollingChecksumTest {
         checksum.getValue();
 
         int actualValue = checksum.getCheckSumValue();
-        int expectedValue = (431 % Constants.MOD_M)+ Constants.MOD_M*(1250 % Constants.MOD_M);
+        int first16Bit = 431 % Constants.MOD_M;
+        int second16Bit = 1250 % Constants.MOD_M;
+        int expectedValue = first16Bit+ (Constants.MOD_M* second16Bit);
         assertEquals(expectedValue,actualValue);
     }
 
@@ -58,10 +60,28 @@ public class RollingChecksumTest {
         checksum.update(block);
         checksum.prune();
 
-        int expectedFirst16 = 431 - 105;
+        int expectedFirst16 = (431 % Constants.MOD_M) - 105;
         int actualFirst16 = checksum.getFirst16Bit();
 
-        int expectedSecond16 = 1250 - (5*105);
+        int expectedSecond16 = (1250 % Constants.MOD_M) - (5*105);
+        int actualSecond16 = checksum.getSecond16Bit();
+
+        assertEquals(expectedFirst16,actualFirst16);
+        assertEquals(expectedSecond16,actualSecond16);
+    }
+
+    @Test
+    public void testRoll(){
+        byte[] block = new byte[]{105,32,97,109,88};
+        RollingChecksum checksum = new RollingChecksum();
+        checksum.update(block);
+        byte nextByte = 100;
+        checksum.roll(nextByte);
+
+        int expectedFirst16 = (((431 % Constants.MOD_M) - 105)+nextByte) % Constants.MOD_M;
+        int actualFirst16 = checksum.getFirst16Bit();
+
+        int expectedSecond16 = ((1250 % Constants.MOD_M) - (5*105)+expectedFirst16) % Constants.MOD_M;
         int actualSecond16 = checksum.getSecond16Bit();
 
         assertEquals(expectedFirst16,actualFirst16);
