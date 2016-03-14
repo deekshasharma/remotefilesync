e2bcf8d254b152b0c65c5ba75769f8c25b4b7e48
Deeksha <deeksha.sharma25@gmail.com>
2016-03-14 10:41:39 -0700

    Comparator changed

--- src/main/java/rsync/WeakChecksumComparator.java
+++ src/main/java/rsync/WeakChecksumComparator.java
@@ -5,9 +5,6 @@ import java.util.Comparator;
 public class WeakChecksumComparator implements Comparator<ChecksumPair> {
 
     public int compare(ChecksumPair pair1, ChecksumPair pair2) {
-        int weak1 = pair1.getWeakChecksum();
-        int weak2 = pair2.getWeakChecksum();
-
-        return(weak1 <= weak2 ?weak1:weak2);
+        return Integer.valueOf(pair1.getWeakChecksum()).compareTo(pair2.getWeakChecksum());
     }
 }
