aef4bdd2a9595559685e6cf2904e6716c6d2aaa7
Deeksha <deeksha.sharma25@gmail.com>
2016-03-21 15:37:08 -0700

    Adding sender and receiver classes

--- src/main/java/rsync/Receiver.java
+++ src/main/java/rsync/Receiver.java
@@ -1,4 +1,28 @@
 package rsync;
 
+import java.io.FileNotFoundException;
+import java.util.List;
+
 public class Receiver {
+
+
+    /**
+     * Receive the sync signal from the other side to calculate ChecksumPair for each non-overlapping block.
+     * @return List<ChecksumPair> containing waek and strong checksum of each block
+     * @throws FileNotFoundException
+     */
+    public List<ChecksumPair> calculateHash() throws FileNotFoundException {
+        GenerateChecksum generateChecksum = new GenerateChecksum();
+        return generateChecksum.getCheckSumPairs(Util.convertToBytes(Constants.RECEIVER_FILEPATH));
+    }
+
+    /**
+     * Receive the Delta/Diff from one side and reconstruct the file
+     * @param delta
+     * @throws FileNotFoundException
+     */
+    public void receiveDifference(String delta) throws FileNotFoundException {
+        Reconstruction fileReconstruct = new Reconstruction();
+        fileReconstruct.reconstructFile(delta,Util.splitIntoBlocks(Util.convertToBytes(Constants.RECEIVER_FILEPATH)));
+    }
 }
--- src/main/java/rsync/Sender.java
+++ src/main/java/rsync/Sender.java
@@ -1,4 +1,16 @@
 package rsync;
 
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Map;
+
 public class Sender {
+
+
+    public String sendDifference(List<ChecksumPair> checksumPairs) throws FileNotFoundException {
+        Map<Integer,Integer> indexTable = Util.buildIndexTable(Util.sortSignaturesFromReceiver(checksumPairs));
+        byte[] senderByteStream = Util.convertToBytes(Constants.SENDER_FILEPATH);
+        Matching matching = new Matching();
+        return matching.calculateDelta(senderByteStream,Util.sortSignaturesFromReceiver(checksumPairs),indexTable);
+    }
 }
