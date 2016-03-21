0f9fdfd15539c66a4794b2e8222f568e314fe1de
Deeksha <deeksha.sharma25@gmail.com>
2016-03-21 15:32:39 -0700

    added paths for remote and local files

--- src/main/java/rsync/Constants.java
+++ src/main/java/rsync/Constants.java
@@ -4,4 +4,7 @@ public class Constants {
     static final int MIN_BLOCK_SIZE_TEST = 5;
     static final int MIN_BLOCK_SIZE = 500;
     static final int MOD_M = 65536;
+    static final String SENDER_FILEPATH = "/tmp/sender.txt";
+    static final String RECEIVER_FILEPATH = "/tmp/receiver.txt";
+
 }
